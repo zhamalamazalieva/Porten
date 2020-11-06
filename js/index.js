57430dceb6a3f54f1bfdb68bbff84001eb0046df
zhamalamazalieva <jamalamazalieva@gmail.com>
2020-11-06 10:54:42 +0100

    added some setting to js using the data-slick-attribute

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-$('.saison__slider').slick({
+$('.slider__item').slick({
   dots:true,
   arrows: false,
-
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
   });
           
